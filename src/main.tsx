af71c9ce45a513ae77d2c968cb74dc47a6ae5599
David Espino <davidespinog@gmail.com>
2023-09-26 13:27:01 -0600

    Add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Pages/App'
 import './index.css'
+import { Provider as ReduxProvider } from 'react-redux'
+import store from './state/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
 )

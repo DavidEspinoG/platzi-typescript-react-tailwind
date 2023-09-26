afc776786b37825e5bb2115e7de1698cf723c369
David Espino <davidespinog@gmail.com>
2023-09-26 17:36:05 -0600

    Add background color to nav bar

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = (): ReactElement => {
   const activeStyle: string = 'underline underline-offset-4'
   const conditionalStyle = ({isActive}:{isActive: boolean}) => isActive ? activeStyle : undefined
   return (
-    <nav className='flex justify-between items-center fixed top-0 z-10 w-full py-5 px-8 text-sm font-light'>
+    <nav className='flex justify-between items-center fixed top-0 z-10 w-full py-5 px-8 text-sm font-light bg-white'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink 
